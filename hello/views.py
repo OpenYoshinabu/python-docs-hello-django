d1162cb4f9c9513f26bfe6a3b6a059ca2bc04df3
Yoshinao Takahashi <yoshinabu@gmail.com>
2021-06-27 12:52:46 +0900

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello, World!\nThis changed is to chekc behaviours on Azure app service. Yay!")
